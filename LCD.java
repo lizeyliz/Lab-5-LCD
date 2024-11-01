10327a35d645ad111166a8e5a92b3837b5265058
lizeyliz <alizaeaton@gmail.com>
2024-11-01 08:47:41 -0700

    setting up

---
+++ LCD.java
@@ -0,0 +1,10 @@
+public class LCD {
+    public static void main (String[] args) {
+        //enter numerator 1, enter denom one
+        //enter numerator 2 enter denom 2
+    }//end main
+    public static int leastCommonDenom(int denom1, int denom2) {
+        int lcd = 0;
+        return lcd;
+    }
+}//end class LCM
