8b38ceb6bd5a8d70ae6b16d766cf9a2f0d9f6ac3
Abby Jahns <2abbylouwho@gmail.com>
2024-11-01 10:31:36 -0700

    added a method with my trying things out, got a version of iteration working.

--- LCD.java
+++ LCD.java
@@ -27,4 +27,38 @@ public class LCD {
         }//end for loop
         return lcd;
     }
+
+    //lease common denominator
+    //enter denominator of first and second fraction
+    public static int lCD(int denom1, int denom2) {
+        //base case denom1 = denom2
+        if (denom1 == denom2) {
+            return denom1;
+        }
+        else {
+            int highestNum = denom1 * denom2; //48
+            int multipleDenom1[];
+            multipleDenom1 = new int[(highestNum/denom1 + 1)]; //array of 12
+            //put multiples of denom1 to highestNum into array
+            for (int i = 0; i < multipleDenom1.length; i++) {
+                multipleDenom1[i] = denom1 * i;
+                System.out.print(multipleDenom1[i] + " ");
+            }
+            System.out.println();
+            //compare multiples of denom2 with array of denom1
+            for (int i = 0; i < (highestNum/denom2); i++) {
+                int multipleDenom2 = denom2 * i;
+                System.out.print(multipleDenom2 + " ");
+                for (int j = 0; j < multipleDenom1.length; j++) {
+                    //multipleDenom1[i] = denom1 * i;
+                    if (multipleDenom1[j] == multipleDenom2 && multipleDenom2 != 0) {
+                        System.out.println("Found");
+                        return multipleDenom2;
+                    }
+                }
+            }//end outside for loop
+            System.out.println();
+            return highestNum; //if none of them match, returnt the highest multiple
+        }//end if else
+    }//end leastCommonDenom method
 }//end class LCM
