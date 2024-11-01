b4bb3f5b15dc813da3d585b27206c3abd46d22f6
lizeyliz <alizaeaton@gmail.com>
2024-11-01 09:18:50 -0700

    trying stuff out

--- LCD.java
+++ LCD.java
@@ -1,3 +1,4 @@
+//wikihow least common denominator
 public class LCD {
     public static void main (String[] args) {
         //enter numerator 1, enter denom one
@@ -5,6 +6,25 @@ public class LCD {
     }//end main
     public static int leastCommonDenom(int denom1, int denom2) {
         int lcd = 0;
+        //limit is denom1*denom2
+        int[] denom1Mults = new int[denom1 * denom2];
+        int[] denom2Mults = new int[denom1 * denom2];
+        //populate denom1Mults array with multiples
+        int multiple = 1;
+        for (int i = 0; i < denom1Mults.length - 1; i++) {
+            denom1Mults[i] = denom1 * multiple;
+            multiple++;
+        }//end for loop
+
+        multiple = 1;
+        //traverse denom1Mults
+        for (int i = 0; i < denom1Mults.length - 1; i++) {
+            if (denom2 = denom1Mults[i]) {
+                denom2 = lcd;
+            } else {
+                
+            }
+        }//end for loop
         return lcd;
     }
 }//end class LCM
